e619f245cc3b6d7f4d346bb07823b7697ab3c11d
ayobisalagi <ayobisalagi@outlook.com>
2024-11-11 09:07:05 +0000

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,11 +1,11 @@
-echo "Download ngrok"
-wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-chmod +x ngrok
-./ngrok authtoken ${{ github.event.inputs.auth }}
-./ngrok tcp 5000 >/dev/null 2>&1 >/dev/null &
+#echo "Download ngrok"
+#wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+#tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+#chmod +x ngrok
+#./ngrok authtoken ${{ github.event.inputs.auth }}
+#./ngrok tcp 5000 >/dev/null 2>&1 >/dev/null &
 
-sleep 5
+#sleep 5
 MYDOCKER=docker$(shuf -i 10000000-99999999 -n 1)
 echo "kunes:kunes:N" > CREATEUSERS.TXT
 docker run --name $MYDOCKER \
